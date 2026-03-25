e6048fb4d7956b64f10c6777674b6f98acb0729c
azacarias-dev <azacarias-2024007@kinal.edu.gt>
2026-03-25 11:57:04 -0600

    Primer hola mundo con react

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,3 +1,10 @@
-export const App = () => {
-  
+const App = () => {
+  return (
+    <>
+      <h1 className="">Hola Mundo</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate, corporis iure repudiandae beatae ratione quis, nisi facilis obcaecati nihil laborum vel. Saepe dolore praesentium dicta cupiditate! Quam, sequi dolorum.</p>
+    </>
+  )
 }
+
+export default App;
