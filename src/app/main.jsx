81f7948ad5bbdf4b9497eaf203b4bc3d4f377f19
azacarias-dev <azacarias-2024007@kinal.edu.gt>
2026-04-20 08:15:12 -0600

    Se crearon rutas para las consultas de el microservicio de autenticacion

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter } from 'react-router-dom';
 import './../styles/index.css'
 import App from './App.jsx'
 
+console.log("Comprobando variables", import.meta.env.VITE_AUTH_URL);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
